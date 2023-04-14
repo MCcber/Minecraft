9b395ee6e6f02c16d18d53767ec30459c995548e
洪荒太初_cber <2682265891@qq.com>
2023-04-14 10:19:33 +0800

    StyleChanged

--- cbhk_environment/GeneralTools/UriHelper.cs
+++ cbhk_environment/GeneralTools/UriHelper.cs
@@ -7,46 +7,6 @@ namespace cbhk_environment.GeneralTools
 {
     public class UriHelper : IMultiValueConverter
     {
-        //public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        //{
-        //    if (value == null) return null;
-        //    Uri uri = (value as BitmapImage).UriSource;
-
-        //    int startIndex = uri.ToString().LastIndexOf('/') + 1;
-        //    int endIndex = uri.ToString().LastIndexOf('.');
-        //    string itemID = uri.ToString().Substring(startIndex, endIndex - startIndex);
-        //    string toolTip = "";
-        //    foreach (var item in MainWindow.ItemDataBase)
-        //    {
-        //        if (item.Key.Substring(0, item.Key.IndexOf(':')) == itemID)
-        //        {
-        //            toolTip = item.Key.Replace(":", " ");
-        //            break;
-        //        }
-        //    }
-        //    return toolTip;
-        //}
-
-        //public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        //{
-        //    if (value == null) return null;
-        //    if (value == null) return null;
-        //    Uri uri = (value as BitmapImage).UriSource;
-        //    int startIndex = uri.ToString().LastIndexOf('/') + 1;
-        //    int endIndex = uri.ToString().LastIndexOf('.');
-        //    string itemID = uri.ToString().Substring(startIndex, endIndex - startIndex);
-        //    string toolTip = "";
-        //    foreach (var item in MainWindow.ItemDataBase)
-        //    {
-        //        if (item.Key.Substring(0, item.Key.IndexOf(':')) == itemID)
-        //        {
-        //            toolTip = item.Key.Replace(":", " ");
-        //            break;
-        //        }
-        //    }
-        //    return toolTip;
-        //}
-
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
             if (values == null) return null;
